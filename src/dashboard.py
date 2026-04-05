b8bc5c6d9d08ad885742f5b3c6f0e3b507c41b11
VollKornBrot127 <philipp.thuemler@outlook.de>
2026-04-05 15:43:28 +0200

    Added SoC warning toast to dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-##################################################################################################################################
+# ================================================================================================================================
 # CREATION DATE:  07.09.2025
 # FILE:           dashboard.py
 # DESCRIPTION:    Dashboard for RCT inverter data.
-##################################################################################################################################
+# ================================================================================================================================
 
-##################################################################################################################################
-# IMPORT MODULES / LIBRARIES                                                                                                     #
-##################################################################################################################################
+# ================================================================================================================================
+# IMPORT MODULES / LIBRARIES
+# ================================================================================================================================
 # Standard library modules
 import logging
-from typing import Any
+import time
 from datetime import datetime
+from typing import Any
 
 # Third-party modules
 import streamlit as st
@@ -20,32 +21,34 @@ from streamlit_autorefresh import st_autorefresh
 # Local application modules
 from rct_inverter_monitor.rct_inverter_monitor import RctInverterMonitor
 
-##################################################################################################################################
-# MARK: GLOBAL VARIABLES                                                                                                               #
-##################################################################################################################################
-LOGGER: logging.Logger = logging.getLogger(__name__)
+# ================================================================================================================================
+# GLOBAL VARIABLES
+# ================================================================================================================================
+logger: logging.Logger = logging.getLogger(__name__)
 DASHBOARD_REFRESH_INTERVAL_MS: int = 5000
 
 
-##################################################################################################################################
-# MARK: FUNCTION IMPLEMENTATION                                                                                                  #
-##################################################################################################################################
-def widget_battery_soc(cache: dict[str, tuple[Any, float]]):
+# ================================================================================================================================
+# FUNCTIONS
+# ================================================================================================================================
+def widget_battery_soc(cache: dict[str, tuple[Any, float]]) -> None:
     st.header("Battery 🔋")
 
     # Battery SoC variables
     battery_soc: float | None = None
     timestamp_battery_soc: str | None = None
-    battery_soc_raw: tuple[Any, float] | None = cache.get("BATTERY_SOC", None)
+    battery_soc_raw: tuple[Any, float] | None = cache.get("BATTERY_SOC")
 
     # Battery power variables
     battery_power: float | None = None
-    battery_power_raw: tuple[Any, float] | None = cache.get("BATTERY_POWER", None)
+    battery_power_raw: tuple[Any, float] | None = cache.get("BATTERY_POWER")
 
     # Get battery SoC and timestamp
     if battery_soc_raw:
         battery_soc = round(battery_soc_raw[0] * 100, 2)
-        timestamp_battery_soc = datetime.fromtimestamp(battery_soc_raw[1]).strftime("%H:%M:%S")
+        timestamp_battery_soc = datetime.fromtimestamp(battery_soc_raw[1]).strftime(
+            "%H:%M:%S"
+        )
 
     # Get battery power and timestamp
     if battery_power_raw:
@@ -81,27 +84,56 @@ def widget_battery_soc(cache: dict[str, tuple[Any, float]]):
         )
         if timestamp_battery_soc:
             st.markdown(f"Last Update: **{timestamp_battery_soc}**")
-        st.badge(label=str(max(st.session_state["battery_soc_values"])), icon="⬆️", color="green")
-        st.badge(label=str(min(st.session_state["battery_soc_values"])), icon="⬇️", color="red")
+        st.badge(
+            label=str(max(st.session_state["battery_soc_values"])),
+            icon="⬆️",
+            color="green",
+        )
+        st.badge(
+            label=str(min(st.session_state["battery_soc_values"])),
+            icon="⬇️",
+            color="red",
+        )
+
+        if battery_soc <= 50.0:
+            st.toast(
+                body=f"Battery SoC is at {battery_soc} %",
+                icon="⚠️",
+                duration="infinite",
+            )
 
 
-def widget_household_load(cache: dict[str, tuple[Any, float]]):
+def widget_household_load(cache: dict[str, tuple[Any, float]]) -> None:
     st.header("Household Load 🏠")
     internal_household_load_power_kw: float | None = None
-    internal_household_load_power_raw: tuple[Any, float] | None = cache.get("HOUSEHOLD_LOAD_INTERNAL", None)
+    internal_household_load_power_raw: tuple[Any, float] | None = cache.get(
+        "HOUSEHOLD_LOAD_INTERNAL"
+    )
 
     # Convert raw value [W] to [kW] and round to 2 decimals
     if internal_household_load_power_raw:
-        internal_household_load_power_kw = round((internal_household_load_power_raw[0] / 1000), 2)
+        internal_household_load_power_kw = round(
+            (internal_household_load_power_raw[0] / 1000), 2
+        )
 
-    if "initial_internal_household_load_power_kw" not in st.session_state and internal_household_load_power_kw is not None:
-        st.session_state["initial_internal_household_load_power_kw"] = internal_household_load_power_kw
+    if (
+        "initial_internal_household_load_power_kw" not in st.session_state
+        and internal_household_load_power_kw is not None
+    ):
+        st.session_state["initial_internal_household_load_power_kw"] = (
+            internal_household_load_power_kw
+        )
 
-    if "initial_internal_household_load_power_kw" in st.session_state and internal_household_load_power_kw is not None:
+    if (
+        "initial_internal_household_load_power_kw" in st.session_state
+        and internal_household_load_power_kw is not None
+    ):
         if "internal_household_load_power_values" not in st.session_state:
             st.session_state["internal_household_load_power_values"] = []
 
-        st.session_state["internal_household_load_power_values"].append(internal_household_load_power_kw)
+        st.session_state["internal_household_load_power_values"].append(
+            internal_household_load_power_kw
+        )
 
         st.metric(
             label="Internal Household Load",
@@ -112,20 +144,29 @@ def widget_household_load(cache: dict[str, tuple[Any, float]]):
         )
 
 
-def widget_solar_generators(cache: dict[str, tuple[Any, float]]):
+def widget_solar_generators(cache: dict[str, tuple[Any, float]]) -> None:
     st.header("Solar Generators 🔆")
     solar_gen_power: float | None = None
     timestamp_solar_gen_power: str | None = None
-    solar_gen_a_power_raw: tuple[Any, float] | None = cache.get("SOLAR_GENERATOR_A_POWER", None)
-    solar_gen_b_power_raw: tuple[Any, float] | None = cache.get("SOLAR_GENERATOR_B_POWER", None)
+    solar_gen_a_power_raw: tuple[Any, float] | None = cache.get(
+        "SOLAR_GENERATOR_A_POWER"
+    )
+    solar_gen_b_power_raw: tuple[Any, float] | None = cache.get(
+        "SOLAR_GENERATOR_B_POWER"
+    )
 
     if solar_gen_a_power_raw and solar_gen_b_power_raw:
-        solar_gen_power = round((solar_gen_a_power_raw[0] + solar_gen_b_power_raw[0]) / 1000, 2)
-        timestamp_solar_gen_power = datetime.fromtimestamp(min(solar_gen_a_power_raw[1], solar_gen_b_power_raw[1])).strftime(
-            "%H:%M:%S"
+        solar_gen_power = round(
+            (solar_gen_a_power_raw[0] + solar_gen_b_power_raw[0]) / 1000, 2
         )
+        timestamp_solar_gen_power = datetime.fromtimestamp(
+            min(solar_gen_a_power_raw[1], solar_gen_b_power_raw[1])
+        ).strftime("%H:%M:%S")
 
-    if "initial_solar_gen_power" not in st.session_state and solar_gen_power is not None:
+    if (
+        "initial_solar_gen_power" not in st.session_state
+        and solar_gen_power is not None
+    ):
         st.session_state["initial_solar_gen_power"] = solar_gen_power
 
     if "initial_solar_gen_power" in st.session_state and solar_gen_power is not None:
@@ -140,50 +181,81 @@ def widget_solar_generators(cache: dict[str, tuple[Any, float]]):
         )
         if timestamp_solar_gen_power:
             st.markdown(f"Last Update: **{timestamp_solar_gen_power}**")
-        st.badge(label=str(max(st.session_state["solar_gen_power_values"])), icon="⬆️", color="green")
-        st.badge(label=str(min(st.session_state["solar_gen_power_values"])), icon="⬇️", color="red")
+        st.badge(
+            label=str(max(st.session_state["solar_gen_power_values"])),
+            icon="⬆️",
+            color="green",
+        )
+        st.badge(
+            label=str(min(st.session_state["solar_gen_power_values"])),
+            icon="⬇️",
+            color="red",
+        )
 
         st.subheader("Solar Genaration Power Values")
-        st.line_chart(st.session_state["solar_gen_power_values"], x_label="Refresh Cycles 🔁", y_label="kW ⚡ ")
+        st.line_chart(
+            st.session_state["solar_gen_power_values"],
+            x_label="Refresh Cycles 🔁",
+            y_label="kW ⚡ ",
+        )
 
 
-##################################################################################################################################
-# MARK: SCRIPT IMPLEMENTATION                                                                                                    #
-##################################################################################################################################
+# ================================================================================================================================
+# SCRIPT IMPLEMENTATION
+# ================================================================================================================================
+
 if __name__ == "__main__":
     logging.basicConfig(
-        filename="dashboard.log", level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        filename="dashboard.log",
+        level=logging.WARNING,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
     refresh_counter: int = st_autorefresh(interval=DASHBOARD_REFRESH_INTERVAL_MS)
     st.set_page_config(page_title="RCT Dashboard", page_icon="☀️", layout="wide")
+    cache: dict[str, tuple[Any, float]] = {}
 
-    if "rct_client" not in st.session_state:
-        st.session_state["rct_client"] = RctInverterMonitor()
-        st.session_state["rct_client"].connect()
-        st.session_state["oid_keys"] = [
-            "BATTERY_SOC",
-            "BATTERY_POWER",
-            "SOLAR_GENERATOR_A_POWER",
-            "SOLAR_GENERATOR_B_POWER",
-            "HOUSEHOLD_LOAD_INTERNAL",
-            "GRID_FEED_YEAR_SUM",
-            "BATTERY_CURRENT",
-        ]
-        st.session_state["rct_client"].start_polling(keys=st.session_state["oid_keys"])
+    # Initialize RCT inverter monitor
+    if "rct_inverter_monitor" not in st.session_state:
+        with st.spinner(text="Connecting to RCT inverter..."):
+            st.session_state.rct_inverter_monitor = RctInverterMonitor()
+            st.session_state.rct_inverter_monitor.connect()
+            st.session_state.oid_keys = [
+                "BATTERY_SOC",
+                "BATTERY_POWER",
+                "SOLAR_GENERATOR_A_POWER",
+                "SOLAR_GENERATOR_B_POWER",
+                "HOUSEHOLD_LOAD_INTERNAL",
+                "GRID_FEED_YEAR_SUM",
+                "BATTERY_CURRENT",
+                "DEVICE_NAME",
+            ]
+            st.session_state.rct_inverter_monitor.start_polling(
+                keys=st.session_state.oid_keys
+            )
+            time.sleep(1.0)
 
-    cache: dict[str, tuple[Any, float]] = st.session_state["rct_client"].get_cache()
+    cache = st.session_state.rct_inverter_monitor.get_cache()
 
-    st.set_page_config(layout="wide")
-    st.markdown("<h1 style='text-align: center'>RCT Dashboard ⚡</h1>", unsafe_allow_html=True)
-    st.markdown(f"**Current Time: {datetime.now().strftime('%H:%M:%S')}**")
+    if cache:
+        st.set_page_config(layout="wide")
+        st.markdown(
+            "<h1 style='text-align: center'>RCT Dashboard ⚡</h1>",
+            unsafe_allow_html=True,
+        )
+        if "DEVICE_NAME" in cache and cache.get("DEVICE_NAME"):
+            st.markdown(
+                f"<h2 style='text-align: center'>{cache.get('DEVICE_NAME', tuple(''))[0]}</h2>",
+                unsafe_allow_html=True,
+            )
+        st.markdown(f"**Current Time: {datetime.now().strftime('%H:%M:%S')}**")
 
-    col1, col2, col3 = st.columns(3)
+        col1, col2, col3 = st.columns(3)
 
-    with col1:
-        widget_battery_soc(cache=cache)
+        with col1:
+            widget_battery_soc(cache=cache)
 
-    with col2:
-        widget_household_load(cache=cache)
+        with col2:
+            widget_household_load(cache=cache)
 
-    with col3:
-        widget_solar_generators(cache=cache)
+        with col3:
+            widget_solar_generators(cache=cache)
